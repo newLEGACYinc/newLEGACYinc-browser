4e7fd5d017f3d2c324430e14a2245f8e0a8921ca
Scott Walters <srwalte2@illinois.edu>
2013-11-25 01:40:58 -0600

    Added secrets.js to gitignore

--- background.js
+++ background.js
@@ -1,10 +1,12 @@
+// http://developer.chrome.com/extensions/notifications.html#type-NotificationOptions
 var opt = {
-    type: "basic",
-    title: "Primary Title",
-    message: "Primary message to display",
-    iconUrl: "imgs/notification.png"
+	type : "basic",
+	title : "newLEGACYinc",
+	message : "Test message",
+	contextMessage : "This is more message",
+	iconUrl : "imgs/notification.png",
 };
 
-chrome.notifications.create("id1", opt, function(id){
-    console.log("Successfully created id1");
+chrome.notifications.create("id1", opt, function(id) {
+	console.log("Successfully created id1");
 });
