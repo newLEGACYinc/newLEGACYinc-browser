90b848b2cc40bcbc332793216cf77679512f60cf
Scott Walters <srwalte2@illinois.edu>
2013-11-25 02:13:14 -0600

    Basic Twitch notification working

---
+++ background/twitch.js
@@ -0,0 +1,31 @@
+var TWITCH_USERNAME = "newLEGACYinc";
+var NOTIFICATION_ID = 'twitch';
+
+var requestUrl = "https://api.twitch.tv/kraken/streams/" + TWITCH_USERNAME + "?client_id=" + TWITCH_CLIENT_ID;
+var streamUrl = "http://www.twitch.tv/" + TWITCH_USERNAME;
+
+var xhr = new XMLHttpRequest();
+xhr.onreadystatechange = function() {
+	if (xhr.readyState == 4) {
+		var json = JSON.parse(xhr.responseText);
+		if (json.stream != null) {
+			serveNotification(json.stream);
+		}
+	};
+};
+xhr.open("GET", requestUrl, true);
+xhr.send();
+
+function serveNotification(stream) {
+	var opt = {
+		type : "basic",
+		title : "newLEGACYinc",
+		message : "Stream online!",
+		contextMessage : "This is more message",
+		iconUrl : "imgs/notification.png",
+	};
+
+	chrome.notifications.create(NOTIFICATION_ID, opt, function(id) {
+		console.log("Successfully created id1");
+	});
+}
