61b6690fc9d7f7cc15d23f0408161e96545c5c37
Scott Walters <srwalte2@illinois.edu>
2013-12-27 00:33:07 -0600

    Added Options menu (that doesn't do anything)
    None of the options have any effect (yet)

---
+++ js/options.js
@@ -0,0 +1,27 @@
+function save() {
+	var youTube_notify = $('#youtube').prop('checked');
+	var twitch_notify = $('#twitchtv').prop('checked');
+	chrome.storage.sync.set({
+		'youtube_notify': youTube_notify,
+		'twitch_notify': twitch_notify
+	}, function done() {});
+}
+
+function load() {
+	chrome.storage.sync.get('youtube_notify', function got(data) {
+		var notify = data.youtube_notify;
+		$('#youtube').prop('checked', notify);
+	});
+	chrome.storage.sync.get('twitch_notify', function got(data) {
+		var notify = data.twitch_notify;
+		$('#twitchtv').prop('checked', notify);
+	})
+}
+
+$(document).ready(function onReady() {
+	load();
+	$('#form').submit(function onSubmit(event) {
+		event.preventDefault();
+		save();
+	})
+})
--- js/setup.js
+++ js/setup.js
@@ -1,9 +1,11 @@
 // This is called when the app or chrome is installed / updated
-chrome.runtime.onInstalled.addListener(function(details) {
-	if (details.reason == 'installed')
+chrome.runtime.onInstalled.addListener(function onInstalled(details) {
+	if (details.reason == 'install')
 		chrome.storage.sync.set({
-			'youtube_last_notified' : moment().unix()
+			'youtube_last_notified': moment().unix(),
+			'youtube_notify': true,
+			'twitch_notify': true
 		}, function() {
-			console.log("Set last notified");
+			console.log("Set initial settings");
 		});
 });
