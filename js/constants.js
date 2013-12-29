f8d3b1951d5922acc44e0318272361a3c0bf45de
Scott Walters <srwalte2@illinois.edu>
2013-12-28 22:16:09 -0600

    Popup now won't open tabs that are already open

--- js/constants.js
+++ js/constants.js
@@ -1,10 +1,15 @@
+// Twitch
 var TWITCH_USERNAME = "newLEGACYinc";
 var TWITCH_URL = 'http://www.twitch.tv/' + TWITCH_USERNAME;
-//var TWITCH_USERNAME = "TSM_TheOddOne";
+var TWITCH_URL_SEARCH_PATTERN = '*://*.twitch.tv/newLEGACYinc*';
 var TWITCH_NOTIFICATION_ID = 'twitch';
 
+// Twitter
 var TWITTER_URL = 'http://twitter.com/newLEGACYinc';
+var TWITTER_URL_SEARCH_PATTERN = '*://*.twitter.com/newLEGACYinc*';
 
+// YouTube
 var YOUTUBE_USERNAME = 'newLEGACYinc';
 var YOUTUBE_NOTIFICATION_ID = 'YouTube';
-var YOUTUBE_URL = 'http://youtube.com/user/newLEGACYinc/videos';
+var YOUTUBE_URL = 'http://www.youtube.com/user/newLEGACYinc/videos';
+var YOUTUBE_URL_SEARCH_PATTERN = '*://*.youtube.com/user/newLEGACYinc*'
--- js/popup.js
+++ js/popup.js
@@ -1,17 +1,30 @@
 $(document).ready(function onReady() {
 	$('#youtube').click(function onClick() {
-		chrome.tabs.create({
-			url: YOUTUBE_URL
-		});
+		createOrActivate(YOUTUBE_URL, YOUTUBE_URL_SEARCH_PATTERN);
 	});
-	$('#twitch').click(function onClick(){
-		chrome.tabs.create({
-			url: TWITCH_URL
-		});
+	$('#twitch').click(function onClick() {
+		createOrActivate(TWITCH_URL, TWITCH_URL_SEARCH_PATTERN);
 	});
-	$('#twitter').click(function onClick(){
-		chrome.tabs.create({
-			url: TWITTER_URL
-		});
+	$('#twitter').click(function onClick() {
+		createOrActivate(TWITTER_URL, TWITTER_URL_SEARCH_PATTERN);
 	});
 });
+
+// if no tab is found with search, create tab with url
+function createOrActivate(url, search) {
+	chrome.tabs.query({
+		url: search
+	}, function callback(tabs) {
+		if (tabs[0]) { // if there is a tab with this url already open
+			// activate the tab
+			chrome.tabs.update(tabs[0].id, {
+				active: true
+			});
+		} else {
+			// create the tab
+			chrome.tabs.create({
+				url: url
+			})
+		}
+	})
+}
